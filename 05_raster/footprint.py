7c6f534b586a03df218904d25038780738585003
rupestre-campos <51023074+rupestre-campos@users.noreply.github.com>
2019-09-24 14:24:23 -0300

    Update footprint.py

--- 05_raster/footprint.py
+++ 05_raster/footprint.py
@@ -1,14 +1,19 @@
 import os
 from subprocess import call
-path = r'D:\Arquivos_GEO_CKC\curso_semad\bases_de_dados\base_raster\LC82180752015243LGN00'
 
-img = 'LC82180752015243LGN00_stack_10m.TIF'
-black = img[:-4]+"_black.tif"
-foot = img[:-4]+"_foot.shp"
+img_path = r'D:\Arquivos_GEO_CKC\curso_semad\bases_de_dados\base_raster\LC82180752015243LGN00'
 
-imgp = os.path.join(path,img)
-blackp = os.path.join(path,black)
-footp = os.path.join(path,foot)
+for root,dirs,files in os.walk(img_path):
+  for img in files:
+    if img.lower().endswith('.tif'):
 
-call("gdal_translate -co compress=lzw -b 1 -ot byte -scale 1 1 {} {}".format(imgp,blackp),shell=True)
-call("gdal_polygonize {} -b 1".format(blackp,footp),shell=True)
+      black = img[:-4]+"_black.tif"
+      foot = img[:-4]+"_foot.shp"
+
+      imgp = os.path.join(root,img)
+      blackp = os.path.join(root,black)
+      footp = os.path.join(root,foot)
+
+      call("gdal_translate -co compress=lzw -b 1 -ot byte -scale 1 1 {} {}".format(imgp,blackp),shell=True)
+      call("gdal_polygonize {} -b 1".format(blackp,footp),shell=True)
+      call('del /q {}'.format(blackp),shell=True)
